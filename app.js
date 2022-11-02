6e22165482df12dbc98fc9f11c84301a600c572d
Hamza-Zaidi619 <117271536+Hamza-Zaidi619@users.noreply.github.com>
2022-11-02 16:31:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// alert("hello")
+// var num1 = 3;
+// var num2 = 4;
+// var sum = num1 + 5-num2;
+// console.log(sum)
+// var percentage =prompt("write your percentage here")
+// var cityname = prompt("enter city name")
+// if(cityname === "karachi"){
+//     alert("Welcome To The City Of Dakaits")
+// if(cityname === "lahore")    
+// }
+// else{
+//     alert("you are good to goo..")
+// }
+
+
+// var c = 5;
+// var d = 5;
+// var x = 6;
+// var y = 6
+// var g = 8
+// var h = 10
+
+// if (c === d) {
+//  if (x === y) {
+//  g = h;
+//  }
+//      }
+//      document.write(g)
+
+// var a = 3;
+// var b = 2;
+// var x = 4;
+// var y = 4;
+// var e = 7;
+// var f = 9;
+
+// if (a === b) {
+// if (x === y) {
+// e = f;
+// }
+// }
+
+// document.write(e)
+
+
+// var city1 = "karachi";
+// var city2 = "lahore";
+// var city3 = "islamabad";
+// var city4 = "pindi";
+
+// var arr = ["karachi","lahore","islamabad","pindi"]
+// console.log(arr[3])
+// arr[0]="naran"
+// console.log(arr)
